3387817e5521adc0aefc0d5196dff073c90aa8cd
Pat Murphy <pmurphy@mobitor.com>
2013-01-16 16:01:41 -0800

    Change

---
+++ AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MBMCharts
+//
+//  Created by Pat Murphy on 12/13/12.
+//  Copyright (c) 2012 Pat Murphy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+{
+    UINavigationController *navigationController;
+}
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
