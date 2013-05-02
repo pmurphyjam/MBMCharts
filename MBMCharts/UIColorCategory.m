e9facd48ab3ef51a357945eb37444e4d56c86cf9
pmurphyjam <pmurphyjam@me.com>
2013-05-01 22:13:51 -0700

    Update UIColorCategory.m

--- MBMCharts/UIColorCategory.m
+++ MBMCharts/UIColorCategory.m
@@ -1,8 +1,8 @@
 //
 //  UIColorCategory.m
-//  InventoryManagement
 //
-//  Copyright 2011 Mobitor. All rights reserved.
+//  Created by Pat Murphy on 12/13/12.
+//  Copyright (c) 2012 Pat Murphy. All rights reserved.
 //
 
 #import "UIColorCategory.h"
